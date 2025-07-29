0ac14399b31d56f6e2e9a1e001cc7f201de9c226
amitdev00 <officialamit1229@gmail.com>
2025-07-29 14:15:20 +0530

    added new files

---
+++ GUI/Application/Pages/page2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("This is a page 2")
+st.write("gkjfkdsj")
--- GUI/Application/streamlit_app.py
+++ GUI/Application/streamlit_app.py
@@ -1,25 +1,39 @@
-import streamlit as st
+# import streamlit as st
 
-# Initialize session state
-if "page" not in st.session_state:
-    st.session_state.page = "home"
+# # Initialize session state
+# if "page" not in st.session_state:
+#     st.session_state.page = "home"
 
-# Navigation function
-def go_to_page(page_name):
-    st.session_state.page = page_name
+# # Navigation function
+# def go_to_page(page_name):
+#     st.session_state.page = page_name
 
-# Home Page
-if st.session_state.page == "home":
-    st.title("🏠 Home Page")
-    st.write("Welcome to the Home Page!")
+# # Home Page
+# if st.session_state.page == "home":
+#     st.title("Home Page")
+#     st.write("Welcome to the Home Page!")
 
-    if st.button("Go to Page 2"):
-        go_to_page("page2")
+#     if st.button("Go to Page 2"):
+#         go_to_page("page2")
 
-# Page 2
-elif st.session_state.page == "page2":
-    st.title("📄 Page 2")
-    st.write("This is Page 2 content.")
+# # Page 2
+# elif st.session_state.page == "page2":
+#     st.title("Page 2")
+#     st.write("This is Page 2 content.")
 
-    if st.button("Back to Home"):
-        go_to_page("home")
+#     if st.button("Back to Home"):
+#         go_to_page("home")
+
+
+import streamlit  as st
+
+st.set_page_config(page_title= "My Streamlit Application", page_icon=":rocket")
+
+st.title("Welcome to my streamlit app")
+st.write("This is a simple streamlit application")
+
+if st.button("Page 1"):
+    st.switch_page("Pages/page1.py")
+    
+elif st.button("Page 2"):
+    st.switch_page("Pages/page2.py")
